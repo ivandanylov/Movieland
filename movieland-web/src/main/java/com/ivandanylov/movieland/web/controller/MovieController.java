272c68a960384c93ec2d3d7df0bbc329fb8f6e05
ivandanylov <prg.ivandanilov@gmail.com>
2018-12-05 09:54:29 +0200

    [lg-1] Application was splitted to modules. Added file appender to logback.

---
+++ movieland-web/src/main/java/com/ivandanylov/movieland/web/controller/MovieController.java
@@ -0,0 +1,48 @@
+package com.ivandanylov.movieland.web.controller;
+
+import com.ivandanylov.movieland.entity.Movie;
+import com.ivandanylov.movieland.service.api.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+    private final static Logger logger = LoggerFactory.getLogger(MovieController.class);
+
+    private MovieService movieService;
+
+    @RequestMapping(path = "/movie", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Movie> getAll() {
+        logger.info("[movieland] Controller path /v1/movie");
+
+        return movieService.getAll();
+    }
+
+    @RequestMapping(path = "/movie/random", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Movie> getRandom() {
+        logger.info("[movieland] Controller path /v1/movie/random");
+
+        return movieService.getRandom();
+    }
+
+    @RequestMapping(path = "/test", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getRoot() {
+        logger.info("[movieland] Controller path /v1/test");
+
+        return "Test";
+    }
+
+    @Autowired
+    public void setMovieService(MovieService movieService) {
+        logger.debug(String.format("[movieland] Movie service is null = %b", movieService == null));
+
+        this.movieService = movieService;
+    }
+}
